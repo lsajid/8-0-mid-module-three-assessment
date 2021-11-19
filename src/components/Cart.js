4a81668bb379acdd3eb1a52b974486ec3e358216
Laiba Sajid Khan <sajid.laiba378@gmail.com>
2021-11-19 16:50:52 -0500

    unordered list works kinda ish

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -5,12 +5,18 @@ class Cart extends Component {
     render () {
         console.log(this.props.cart);
         console.log(this.props.price);
-        
+        let allUnorderedLists = this.props.cart.map((item, index)=> {
+            return (
+                <ul>
+                    <li> yo</li>
+                </ul>
+            )
+        })
         return (
             <div>
                 <h2>Cart</h2>
                 <div>
-                 <li> item Name:</li>
+                 {allUnorderedLists}
                 </div>
                 <h3>Subtotal: {formatPrice(this.props.price)}</h3>
                 <h3>Tax: {formatPrice(this.props.price*0.05)} </h3>
